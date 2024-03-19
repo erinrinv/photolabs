811246049302f9b7df765ca0ca0e296d50a78058
Erin Von Gertzen <ianvon@shaw.ca>
2024-03-19 13:27:21 -0600

    Add PhotolistItem component and refactored it and added it to app.jsx and displayed photo on the homepage

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -14,8 +14,17 @@ const sampleDataForPhotoListItem = {
   profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
 };
 
-const PhotoListItem = () => {
-  /* Insert React */
+const PhotoListItem = ({ id, location, imageSource, username, profile }) => {
+  return (
+    <div className="photo-list-item">
+      <img src={imageSource} alt={`Photo by ${username}`} />
+      <div className="user-details">
+        <img src={profile} alt={`Profile of ${username}`} className="profile-pic" />
+        <div className="username">{username}</div>
+      </div>
+      <div className="location">{location.city}, {location.country}</div>
+    </div>
+  );
 };
 
 export default PhotoListItem;
