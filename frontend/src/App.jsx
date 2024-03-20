14d74ba231af30d904abdaba68dab181b5a55ce8
Erin Von Gertzen <ianvon@shaw.ca>
2024-03-20 16:51:37 -0600

    created implemented top nav bar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 
-import PhotoListItem from './components/PhotoListItem';
-import TopicList from 'components/TopicList';
+import TopNavigationBar from 'components/TopNavigationBar';
 import PhotoList from 'components/PhotoList';
 import './App.scss';
 
@@ -14,8 +13,8 @@ const App = () => {
   
   return (
     <div className="App">
+    <TopNavigationBar/>
     <PhotoList/>
-    <TopicList/>
   </div>
   );
 };
